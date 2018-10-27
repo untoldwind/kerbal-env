cfdfed8e0ab49003a56fb4558d1e1fe55e4b54e1
Bodo Junglas <junglas@objectcode.de>
2018-10-27 20:18:20 +0200

    Add USI-LS

--- lib/receips/USITools.py
+++ lib/receips/USITools.py
@@ -9,20 +9,21 @@ class Receipt:
         self.game_dir = game_dir
         self.project_dir = project_dir
         self.source_dir = SourceDir(game_dir, project_dir.joinpath("USITools", "USITools"))
-        self.source_dir.output = project_dir.joinpath("FOR_RELEASE", "GameData", "000_USITools", "USITools.dll")
+        self.for_release_dir = project_dir.joinpath("FOR_RELEASE", "GameData", "000_USITools")
+        self.source_dir.output = self.for_release_dir.joinpath("USITools.dll")
         self.target_dir = game_dir.joinpath("GameData", "000_USITools")
 
     def build(self):
         logging.info("  Build Release")
-        rm(self.project_dir.joinpath("FOR_RELEASE", "GameData", "000_USITools"), "*.dll")
-        rm(self.project_dir.joinpath("FOR_RELEASE", "GameData", "000_USITools"), "*.mdb")
-        rm(self.project_dir.joinpath("FOR_RELEASE", "GameData", "000_USITools"), "*.pdb")
+        rm(self.for_release_dir, "*.dll")
+        rm(self.for_release_dir, "*.mdb")
+        rm(self.for_release_dir, "*.pdb")
         self.source_dir.std_compile(
             references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
 
     def install(self):
         rm_rf(self.target_dir)
-        shutil.copytree(self.project_dir.joinpath("FOR_RELEASE", "GameData", "000_USITools"), self.target_dir)
+        shutil.copytree(self.for_release_dir, self.target_dir)
 
     def check_installed(self):
         return self.target_dir.exists()
--- lib/receips/USI_LS.py
+++ lib/receips/USI_LS.py
@@ -1,14 +1,30 @@
+import logging
+import shutil
+from lib.exec import SourceDir
+from lib.utils import rm_rf, rm
+
 
 class Receipt:
     def __init__(self, game_dir, project_dir):
         self.game_dir = game_dir
         self.project_dir = project_dir
+        self.source_dir = SourceDir(game_dir, project_dir.joinpath("Source", "USILifeSupport"))
+        self.for_release_dir = project_dir.joinpath("FOR_RELEASE", "GameData", "UmbraSpaceIndustries", "LifeSupport")
+        self.source_dir.output = self.for_release_dir.joinpath("USILifeSupport.dll")
+        self.target_dir = game_dir.joinpath("GameData", "UmbraSpaceIndustries", "LifeSupport")
+        self.usitools_lib = project_dir.parent.joinpath("USITools", "FOR_RELEASE", "GameData", "000_USITools", "USITools.dll")
 
     def build(self):
-        pass
+        logging.info("  Build Release")
+        rm(self.for_release_dir, "*.dll")
+        rm(self.for_release_dir, "*.mdb")
+        rm(self.for_release_dir, "*.pdb")
+        self.source_dir.std_compile(
+            references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll", self.usitools_lib])
 
     def install(self):
-        pass
+        rm_rf(self.target_dir)
+        shutil.copytree(self.for_release_dir, self.target_dir)
 
     def check_installed(self):
-        return False
+        return self.target_dir.exists()
