8613e0c2d9ecc921ff6f48869c8aff75ce0836d9
Bodo Junglas <junglas@objectcode.de>
2018-11-01 23:27:38 +0100

    Add ClickThroughBlocker

---
+++ lib/receips/ClickThroughBlocker.py
@@ -0,0 +1,26 @@
+import logging
+import shutil
+from lib.exec import SourceDir
+from lib.utils import mkdir_p, rm_rf
+
+
+class Receipt:
+    def __init__(self, game_dir, project_dir):
+        self.game_dir = game_dir
+        self.project_dir = project_dir
+        self.source_dir = SourceDir(game_dir, project_dir.joinpath("ClickThroughBlocker" ))
+        self.source_dir.output = project_dir.joinpath("GameData", "000_ClickThroughBlocker", "ClickThroughBlocker.dll")
+        self.target_dir = game_dir.joinpath("GameData", "000_ClickThroughBlocker")
+
+    def build(self):
+        logging.info("  Build Release")
+        self.source_dir.std_compile(
+            references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
+
+    def install(self):
+        rm_rf(self.target_dir)
+        shutil.copytree(self.project_dir.joinpath(
+            "GameData", "000_ClickThroughBlocker"), self.target_dir)
+
+    def check_installed(self):
+        return self.target_dir.exists()
