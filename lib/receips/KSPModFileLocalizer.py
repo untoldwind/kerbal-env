9ccf506e1babfc2caf775178097493f4dd0ca990
Bodo Junglas <junglas@objectcode.de>
2018-11-15 20:15:24 +0100

    Add PlanetaryBaseSystems

---
+++ lib/receips/KSPModFileLocalizer.py
@@ -0,0 +1,30 @@
+import logging
+import shutil
+from lib.exec import SourceDir
+from lib.utils import rm_rf, rm
+
+
+class Receipt:
+    def __init__(self, game_dir, project_dir):
+        self.game_dir = game_dir
+        self.project_dir = project_dir
+        self.source_dir = SourceDir(game_dir, project_dir.joinpath("Sources", "KSPModFileLocalizer"))
+        self.for_release_dir = self.project_dir.joinpath("FOR_RELEASE")
+        self.source_dir.output = self.for_release_dir.joinpath("KSPModFileLocalizer.dll")
+        self.target_file = game_dir.joinpath("GameData", "KSPModFileLocalizer.dll")
+
+    def build(self):
+        logging.info("  Build Release")
+        rm(self.for_release_dir, "*.dll")
+        self.source_dir.std_compile(
+            references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
+
+    def can_install(self):
+        return self.source_dir.output.exists()
+
+    def install(self):
+        rm_rf(self.target_file)
+        shutil.copy(self.source_dir.output, self.target_file)
+
+    def check_installed(self):
+        return self.target_file.exists()
--- lib/receips/PlanetaryBaseSystems.py
+++ lib/receips/PlanetaryBaseSystems.py
@@ -1,17 +1,30 @@
+import logging
+import shutil
+from lib.exec import SourceDir
+from lib.utils import rm_rf, rm
 
 class Receipt:
     def __init__(self, game_dir, project_dir):
         self.game_dir = game_dir
         self.project_dir = project_dir
+        self.source_dir = SourceDir(game_dir, project_dir.joinpath("Sources", "PlanetarySurfaceStructures"))
+        self.for_release_dir = self.project_dir.joinpath("FOR_RELEASE", "GameData", "PlanetaryBaseInc", "BaseSystem", "Plugins")
+        self.source_dir.output = self.for_release_dir.joinpath("PlanetarySurfaceStructures.dll")
+        self.target_dir = game_dir.joinpath("GameData", "PlanetaryBaseInc")
 
     def build(self):
-        pass
+        logging.info("  Build Release")
+        rm(self.for_release_dir, "*.dll")
+        self.source_dir.std_compile(
+            references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
 
     def can_install(self):
-        return True
+        return self.source_dir.output.exists()
 
     def install(self):
-        pass
+        rm_rf(self.target_dir)
+        shutil.copytree(self.project_dir.joinpath("FOR_RELEASE", "GameData", "PlanetaryBaseInc"), self.target_dir)
+        rm(self.target_dir.joinpath("ModSupport", "Parts", "KAS"), "*_LEGACY.cgf")
 
     def check_installed(self):
-        return False
+        return self.target_dir.exists()
