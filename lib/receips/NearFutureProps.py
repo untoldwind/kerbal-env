91503b368fad216cf4d1ab0fe1dc16d61692874e
Bodo Junglas <junglas@objectcode.de>
2018-11-15 19:19:59 +0100

    Add StationPartsExpansionRedux

--- lib/receips/NearFutureProps.py
+++ lib/receips/NearFutureProps.py
@@ -1,18 +1,31 @@
+import logging
+import shutil
+from lib.exec import SourceDir
+from lib.utils import mkdir_p, rm_rf, rm
 
 
 class Receipt:
     def __init__(self, game_dir, project_dir):
         self.game_dir = game_dir
         self.project_dir = project_dir
+        self.source_dir = SourceDir(game_dir, project_dir.joinpath("Source", "NFPropUtils"))
+        self.plugins_dir = project_dir.joinpath("GameData", "NearFutureProps", "Plugins")
+        self.source_dir.output = self.plugins_dir.joinpath("NFPropUtils.dll")
+        self.target_dir = game_dir.joinpath("GameData", "NearFutureProps")
 
     def build(self):
-        pass
+        logging.info("  Build Release")
+        rm(self.plugins_dir, "*.dll")
+        rm(self.project_dir.joinpath("GameData", "NearFutureProps", "Versioning"), "*.dll")
+        self.source_dir.std_compile(
+            references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
 
     def can_install(self):
-        return True
+        return self.source_dir.output.exists()
 
     def install(self):
-        pass
+        rm_rf(self.target_dir)
+        shutil.copytree(self.project_dir.joinpath("GameData", "NearFutureProps"), self.target_dir)
 
     def check_installed(self):
-        return False
+        return self.target_dir.exists()
