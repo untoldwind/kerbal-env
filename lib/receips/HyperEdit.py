75ff974d7e4648dbcee7a38b162bd403f0a13783
Bodo Junglas <junglas@objectcode.de>
2018-11-21 18:21:40 +0100

    Fix HyperEdit install

--- lib/receips/HyperEdit.py
+++ lib/receips/HyperEdit.py
@@ -7,7 +7,7 @@ class HyperEdit(Receipt):
     def __init__(self, game_dir, project_dir):
         super().__init__(game_dir, project_dir)
         self.source_dir = SourceDir(game_dir, project_dir.joinpath("Source"))
-        self.target_dir = game_dir.joinpath("GameData", "Kerbaltek", "HyperEdit")
+        self.target_dir = game_dir.joinpath("GameData", "Kerbaltek")
 
     def build(self):
         output_dir = self.project_dir.joinpath("obj")
@@ -21,9 +21,11 @@ class HyperEdit(Receipt):
         return self.project_dir.joinpath("obj", "HyperEdit.dll").exists()
 
     def install(self):
-        rm_rf(self.target_dir)
         mkdir_p(self.target_dir)
+        mkdir_p(self.target_dir.joinpath("HyperEdit"))
+        rm(self.target_dir, "*.dll")
         shutil.copy(self.project_dir.joinpath("obj", "HyperEdit.dll"), self.target_dir)
+        shutil.copy(self.project_dir.joinpath("HyperEdit.version"), self.target_dir.joinpath("HyperEdit"))
 
     def check_installed(self):
         return self.target_dir.exists()
