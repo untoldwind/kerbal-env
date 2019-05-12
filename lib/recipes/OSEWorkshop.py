9a0a68027bed65ab5f0d7a6c6497072e1b1f4628
Bodo Junglas <junglas@objectcode.de>
2019-05-12 13:25:20 +0200

    Update OSEWorkshop

--- lib/recipes/OSEWorkshop.py
+++ lib/recipes/OSEWorkshop.py
@@ -5,20 +5,23 @@ from lib.utils import mkdir_p, rm_rf, rm
 from lib.recipes import Receipt
 
 class OSEWorkshop(Receipt):
-    depends = ["ModuleManager", "KIS", "FireSpitterCore", "CommunityResourcePack"]
+    depends = ["ModuleManager", "KIS", "FireSpitterCore", "CommunityResourcePack", "ClickThroughBlocker"]
 
     def __init__(self, game_dir, project_dir):
         super().__init__(game_dir, project_dir)
-        self.source_dir = SourceDir(game_dir, project_dir.joinpath("Source", "Workshop"))
+        self.source_dir = SourceDir(game_dir, project_dir.joinpath("Workshop"))
         self.source_dir.output = project_dir.joinpath("GameData", "Workshop", "Plugins", "Workshop.dll")
         self.target_dir = game_dir.joinpath("GameData", "Workshop")
+        self.clickthrouh_lib = project_dir.parent.joinpath("ClickThroughBlocker", "GameData", "000_ClickThroughBlocker", "ClickThroughBlocker.dll")
+        self.kis_lib = project_dir.parent.joinpath("KIS", "Binaries", "KIS.dll")
 
     def build(self):
         logging.info("  Build Release")
         rm(self.project_dir.joinpath("GameData", "Workshop", "Plugins"), "*.dll")
+        mkdir_p(self.project_dir.joinpath("GameData", "Workshop", "Plugins"));
         self.source_dir.std_compile(
             exclude="docs_project/**/*.cs",
-            references=["Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
+            references=[self.kis_lib, self.clickthrouh_lib, "Assembly-CSharp.dll", "Assembly-CSharp-firstpass.dll", "UnityEngine.dll", "UnityEngine.UI.dll"])
 
     def can_install(self):
         return self.source_dir.output.exists()
