48cadcdc05717dad0858e5dfbca2d91bfbac3d7b
Bodo Junglas <junglas@objectcode.de>
2023-02-25 13:21:07 +0100

    Fix EVE/scatterer

--- lib/recipes/EVERedux.py
+++ lib/recipes/EVERedux.py
@@ -4,20 +4,20 @@ from lib.exec import SourceDir
 from lib.utils import mkdir_p, rm_rf
 from lib.recipes import Receipt
 
-class scatterer(Receipt):
+class EVERedux(Receipt):
     def __init__(self, game_dir, project_dir):
         super().__init__(game_dir, project_dir)
-        self.target_dir = game_dir.joinpath("GameData", "Scatterer")
+        self.target_dir = game_dir.joinpath("GameData", "EnvironmentalVisualEnhancements")
 
     def build(self):
         pass
-    
+
     def can_install(self):
         return True
 
     def install(self):
         rm_rf(self.target_dir)
-        shutil.copytree(self.project_dir.joinpath("GameData", "Scatterer"), self.target_dir)
+        shutil.copytree(self.project_dir.joinpath("GameData", "EnvironmentalVisualEnhancements"), self.target_dir)
 
     def check_installed(self):
         return self.target_dir.exists()
